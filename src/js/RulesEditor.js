7ba5d5fc2fd24f71714a0e15c5e8e63ea99bda3e
Oren Nachman <code@nachmore.com>
2018-07-09 13:15:17 +1000

    Functional structure for RulesEditor with ties back to the UI

--- src/js/RulesEditor.js
+++ src/js/RulesEditor.js
@@ -1,55 +1,88 @@
-var RulesEditor = new function() {
+var RulesEditor = RulesEditor || function() {};
 
-  this.elems = {};
+RulesEditor.prototype = {
 
-  this.playerActions = {
+  elems: {},
+
+  playerActions: {
     Player: {
       Jump: 0,
       Attack: 0,
     }
-  }
+  },
 
-  this.validTriggers = {
-    Game: {
-      Load: 0,
-      Bloat: 0,
-    },
-    "Human Player": {
-      "Presses Up": this.playerActions,
-      "Presses Down": this.playerActions,
-      "Presses Left": this.playerActions,
-      "Presses Right": this.playerActions
-    },
-    "Ninja": {
-      Jumps: 0,
-      Attacks: 0,
-      "Gets Hit By Zombie": 0,
-      "Hits an Object": 0,
-    },
-    "Zombie": {
-      "Attacks": 0,
-      "Gets Hit By Player": 0
-    }
-  }
+  validTriggers: {},
 
-  this.init = function() {
+  get curActor()     {return this.elems.selectActor[this.elems.selectActor.selectedIndex].text;},
+  get curSituation() {return this.elems.selectSituation[this.elems.selectSituation.selectedIndex].text;},
+  get curTarget()    {return this.elems.selectTarget[this.elems.selectTarget.selectedIndex].text;},
+  get curAction()    {return this.elems.selectAction[this.elems.selectAction.selectedIndex].text; },
+
+  init: function() {
+
+    this.initValidTriggers();
 
     this.elems.selectActor     = document.getElementById("actor");
     this.elems.selectSituation = document.getElementById("situation");
+    this.elems.selectTarget    = document.getElementById("target");
+    this.elems.selectAction     = document.getElementById("action");
 
+    this.elems.selectActor.onchange     = function() {RulesEditor.updateSituations();};
+    this.elems.selectSituation.onchange = function() {RulesEditor.updateTargets();};
+    this.elems.selectTarget.onchange    = function() {RulesEditor.updateActions();}
+
+    // this will set off an onchange() cascade
     this.initSelect(this.elems.selectActor, this.validTriggers);
+  },
 
-    this.elems.selectActor.onchange = function() {RulesEditor.updateActorActions();};
-    this.updateActorActions();
-  }
+  // init once the object is constructed so that we have access to internal properties such as playerActions
+  initValidTriggers: function() {
+    
+    this.validTriggers = {
+      Game: {
+        Load: 0,
+        Bloat: 0,
+      },
+      "Human Player": {
+        "Presses Up": {
+          Player: {
+            Jump: {
+              apply: function() {},
+              undo: function() {}
+            },
+            Attack: null,
+          },
+        },
+        "Presses Down": RulesEditor.playerActions,
+        "Presses Left": RulesEditor.playerActions,
+        "Presses Right": RulesEditor.playerActions
+      },
+      "Ninja": {
+        Jumps: 0,
+        Attacks: 0,
+        "Gets Hit By Zombie": 0,
+        "Hits an Object": 0,
+      },
+      "Zombie": {
+        "Attacks": 0,
+        "Gets Hit By Player": 0
+      }
+    };
+  },
 
-  this.updateActorActions = function() {
-    var selected = this.elems.selectActor[this.elems.selectActor.selectedIndex].text;
+  updateSituations: function() {
+    this.initSelect(this.elems.selectSituation, this.validTriggers[this.curActor]);
+  },
 
-    this.initSelect(this.elems.selectSituation, this.validTriggers[selected]);
-  }
+  updateTargets: function() {
+    this.initSelect(this.elems.selectTarget, this.validTriggers[this.curActor][this.curSituation]);
+  },
 
-  this.initSelect = function(select, options) {
+  updateActions: function() {
+    this.initSelect(this.elems.selectAction, this.validTriggers[this.curActor][this.curSituation][this.curTarget]);
+  },
+
+  initSelect: function(select, options) {
     select.innerHTML = "";
 
     for (var option in options) {
@@ -58,5 +91,10 @@ var RulesEditor = new function() {
 
       select.add(elem);
     }
+
+    if (typeof select.onchange == "function")
+      select.onchange.apply(select);    
   }
 }
+
+RulesEditor = new RulesEditor();
